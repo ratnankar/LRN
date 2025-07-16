a89b96394b87411bd1a31c73f32b574b71a3427d
ratnankar <ratnankar0220@gmail.com>
2025-07-16 20:39:58 +0530

    Updated

--- h.cpp
+++ h.cpp
@@ -4,17 +4,26 @@ using namespace std;
 class H{
 
     public:
-    
+
         static int b; 
     
+        static void k(){
+
+            cout<<"Static member function can only call static variables";
+
+        }
 };
 
-int H::b;
+int H::b = 2;
 
 int main(){
 
+    H::k();
+
     H obj;
 
-    cout<<obj.b;
+    H::b = 5;
+
+    cout<<endl<<obj.b;
 
 }
