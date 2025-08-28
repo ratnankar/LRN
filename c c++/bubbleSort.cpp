3df90b68027be4e9cb6ee6817138f7aca6662fa5
ratnankar <ratnankar0220@gmail.com>
2025-08-28 23:09:53 +0530

    bubbleSort

--- c c++/bubbleSort.cpp
+++ c c++/bubbleSort.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main(){
 
-    int a[6] = {6,5,4,3,2,1};
+    int a[6] = {1,2,3,4,5,0};
 
     for(int i = 0; i<5; i++){
 
+        bool flag = false;
+            
         for(int j = 5; j>0; j--){
 
             if(a[5-j]>a[5-(j-1)]){
@@ -16,9 +18,21 @@ int main(){
                 a[5-j] = a[5-(j-1)];
 
                 a[5-(j-1)] = k;
+
+                flag = true;
             }
+        }
+
+        if(flag==false&&i==0){
+
+            cout<<"Already Sorted"<<endl;
+            break;
+        }
+        if(flag==false){
+
+            break;
         }
     }
 
-    cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4]<<" "<<a[5]<<" "<<a[6];
+    cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4]<<" "<<a[5];
 }
